1a24886bd4d3c496cc6fd37bf818e8fed7225353
PruthvirajGire18 <girepruthviraj@gmail.com>
2025-11-27 21:34:10 +0530

    url changed

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ const app = express();
 connectDB();
 
 // Middlewares
-app.use(cors({ origin: "http://localhost:5173", credentials: true }));
+app.use(cors({ origin: "https://daxyassignment.netlify.app", credentials: true }));
 app.use(express.json());
 
 // Routes
